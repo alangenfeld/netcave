5700534522afbebc26ce172fabe82adf86af2c07
Alex Langenfeld <alangenfeld@wisc.edu>
2011-04-05 12:14:17 -0500

    Setup netcave to use the workaround. get vs new dungeon added to save time

--- stupidDungeon.py
+++ stupidDungeon.py
@@ -1,6 +1,6 @@
 import pickle, subprocess
 
-def getDungeon(depth,width=64,height=64,rooms=50,py3x="C:\python31\python.exe"):
+def newDungeon(depth,width=64,height=64,rooms=50,py3x="C:\python31\python.exe"):
     '''uses pyDungeon and a version of python with numpy to generate a new random dungeon
 
 depth - dungeon depth
@@ -20,6 +20,15 @@ py3x - path to python with numpy installed, defaults to "C:\python31\python.exe"
     subprocess.call([py3x,'dumpDungeon.py','tempDungeon.pkl',repr(depth),repr(width),repr(height),repr(rooms)])
     dungeon = pickle.load(open('tempDungeon.pkl','rb'))
     return dungeon
+
+def getDungeon():
+''' 
+Return the current tempDungeon.pkl
+'''
+    dungeon = pickle.load(open('tempDungeon.pkl','rb'))
+    return dungeon
+
+
 
 if __name__=="__main__":
     print(getDungeon(1,rooms=35))
