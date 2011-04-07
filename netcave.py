646c7b90d014bf2670fe05670d24f415c51ce8b0
Alex Langenfeld <alangenfeld@wisc.edu>
2011-04-07 11:43:03 -0500

    make some fixes

--- netcave.py
+++ netcave.py
@@ -19,22 +19,26 @@ floor.renderable('').show()
 
 phys = VRScript.Core.Physical('Floor', fbox)
 phys.setPhysicsProperties(VRScript.Core.PhysicsProperties(0,.1,.1,.1,.2))
-phys.setConstraints(VRScript.Math.Vector(1,1,1), VRScript.Math.Vector(1,1,1))
-phys.setCollisionType(VRScript.Core.CollisionType.Dynamic)
+phys.setCollisionType(VRScript.Core.CollisionType.Static)
 floor.attach(phys)
 
+# insert floor 
 
 for y in range(len(dungeon)) :
     for x in range(len(dungeon[y])) :
         if dungeon[y][x] == 1 :
             name = 'cube[' + str(y) + '][' + str(x) + ']'
             cube_e = VRScript.Core.Entity(name)
+            # translate and attach mesh
             cube_m = VRScript.Resources.Box(VRScript.Math.Vector(1,1,1)*(CAVE/2),
-                                            VRScript.Math.Point((x*CAVE) + CAVE/2,
-                                                                (y*CAVE) + CAVE/2,
+                                            VRScript.Math.Point(((x*CAVE) + CAVE/2) - HEIGHT*CAVE/2,
+                                                                ((y*CAVE) + CAVE/2) - WIDTH*CAVE/2,
                                                                 CAVE/2))
             
             cube_e.attach(VRScript.Core.Renderable(name,cube_m))
             cube_e.renderable('').show()
-            cube_e.attach(phys)
+            cube_p = VRScript.Core.Physical(name, cube_m)
+            cube_p.setPhysicsProperties(VRScript.Core.PhysicsProperties(0,.1,.1,.1,.2))
+            phys.setCollisionType(VRScript.Core.CollisionType.Static)
+            cube_e.attach(cube_p)
             
